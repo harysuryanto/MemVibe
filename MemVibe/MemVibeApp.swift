4f05c0ab2b794aa776cae64a80ed6d408c6b99c6
Hary Suryanto <hary.suryanto01@gmail.com>
2026-03-02 05:42:13 +0700

    Initial Commit

---
+++ MemVibe/MemVibeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemVibeApp.swift
+//  MemVibe
+//
+//  Created by Hary Suryanto on 02/03/26.
+//
+
+import SwiftUI
+
+@main
+struct MemVibeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
